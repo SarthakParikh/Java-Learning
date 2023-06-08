2f4981e6a829c58684df9b931eebb1f46242fbdd
Sarthak Parikh <sarthakparikh20010409@gmail.com>
2023-06-09 00:59:51 +0530

    datatyes

---
+++ CORE JAVA/DataTypes.java
@@ -0,0 +1,88 @@
+import java.lang.*;
+public class DataTypes {
+    public static void main(String args[]){
+// integer max value 
+        System.out.println("Int Minium Value :" + Integer.MIN_VALUE );
+        System.out.println("Int Maximum Value :" + Integer.MAX_VALUE );
+        System.out.println("Int Byte Value :" + Integer.BYTES );
+
+// long  min-max value
+System.out.println("Long Minium Value :" + Long.MIN_VALUE );
+System.out.println("Long Maximum Value :" + Long.MAX_VALUE );
+System.out.println("Long Byte Value :" + Long.BYTES );
+
+// Byte  min-max value
+System.out.println("Byte Minium Value :" + Byte.MIN_VALUE );
+System.out.println("Byte Maximum Value :" + Byte.MAX_VALUE );
+System.out.println("Byte Byte Value :" + Byte.BYTES );
+
+// Short  min-max value
+System.out.println("Short Minium Value :" + Short.MIN_VALUE );
+System.out.println("Short Maximum Value :" + Short.MAX_VALUE );
+System.out.println("Short Byte Value :" + Short.BYTES );
+
+// Float  min-max value
+System.out.println("Float Minium Value :" + Float.MIN_VALUE );
+System.out.println("Float Maximum Value :" + Float.MAX_VALUE );
+System.out.println("Float Byte Value :" + Float.BYTES );
+
+// Doubble  min-max value
+System.out.println("Double Minium Value :" + Double.MIN_VALUE );
+System.out.println("Double Maximum Value :" + Double.MAX_VALUE );
+System.out.println("Double Byte Value :" + Double.BYTES );
+
+// Datatype Details
+
+int a = 5;
+int b = -5;
+
+System.out.println(" Possitive 5 in binary :"+Integer.toBinaryString(a));
+System.out.println(" Nagitive 5 in binary :"+Integer.toBinaryString(b));
+
+    }
+    
+}
+/*
+ * Data Types and Variable 
+ * 
+ * Variables - ment for string data
+ * variable contains pre define data type
+ * 1)  Primiter Data type
+ * 
+ * 
+ *      Integer -    Byte   -- Size 1 -- Range -> -128 to 127 
+ *                   Short  -- size 2 -- Range-32768 to 32787
+ *                   int   -- Size 4  -- range 
+ *                   long  -- Size 8
+ *      Flot ---  float -- size -4  after decimal 6 digit
+ *                 doubble - Size 8  after decimal more then 6 digit
+ *      Char -- Size 2 byte
+ *      bolline -- true or false value 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  variabe are used to store data 
+ * 
+ * 
+ * rules of variable name
+ *      -- variale names are case sensetive
+ *          containe alphabets numbes _ and $ 
+ *          should not have a key word
+ *          no classs name
+ *          end with semi cooolo
+ *          can not start with number
+ * 
+ * 
+ * 
+ * 
+ * Literals are consten value used in program
+ *  
+ * 
+ * 
+ *  
+ * 
+ * 
+ */
